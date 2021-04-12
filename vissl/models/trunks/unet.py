cd0fba236c9617d6ebb9fbfb311e5e0068ff020e
david schmidlin <david.schmidlin@gmail.com>
2021-04-11 18:21:24 -0700

    working on getting unet setup wit _feature_blocks and feat_eval_mapping

--- vissl/models/trunks/unet.py
+++ vissl/models/trunks/unet.py
@@ -4,12 +4,17 @@ Copyright (c) Facebook, Inc. and its affiliates.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
-
+from typing import List
 import torch
 from torch import nn
 from torch.nn import functional as F
 from vissl.config import AttrDict
 from vissl.models.trunks import register_model_trunk
+from vissl.models.model_helpers import (
+    Flatten,
+    get_trunk_forward_outputs,
+    transform_model_input_data_type
+)
 
 # Unet from FastMRI
 @register_model_trunk("unet")
@@ -66,44 +71,88 @@ class Unet(nn.Module):
                 nn.Conv2d(ch, self.out_chans, kernel_size=1, stride=1),
             )
         )
+        
+        # we mapped the layers of resnet model into feature blocks to facilitate
+        # feature extraction at various layers of the model. The layers for which
+        # to extract features is controlled by requested_feat_keys argument in the
+        # forward() call.
+        self._feature_blocks = nn.ModuleDict()
+        for i in range(self.down_sample_layers.__len__()):
+            self._feature_blocks.append((f"downlayer{i+1}", self.down_sample_layers[i] ))
+        
+        self._feature_blocks.append( ('conv', self.conv) )
+        
+        for i in range(self.up_transpose_conv.__len__()):
+            self._feature_blocks.append((f"uptranconvlayer{i+1}", self.up_transpose_conv[i]))
+        
+        for i in range(self.up_conv.__len__()):
+            self._feature_blocks.append((f"upconvlayer{i+1}", self.up_conv[i]))
+        
+        self._feature_blocks.append( ("flatten",Flatten(1)) )
+        
+        # give a name mapping to the layers so we can use a common terminology
+        # across models for feature evaluation purposes.
+        self.feat_eval_mapping = {
+            "conv1": "conv1_relu",
+            "res1": "maxpool",
+            "res2": "layer1",
+            "res3": "layer2",
+            "res4": "layer3",
+            "res5": "layer4",
+            "res5avg": "avgpool",
+            "flatten": "flatten",
+        }
+        
+    def forward(
+        self, x: torch.Tensor, out_feat_keys: List[str] = None
+    ) -> List[torch.Tensor]:
+        feat = transform_model_input_data_type(x, self.model_config)
+        return get_trunk_forward_outputs(
+            feat,
+            out_feat_keys=out_feat_keys,
+            feature_blocks=self._feature_blocks,
+            feature_mapping=self.feat_eval_mapping,
+            use_checkpointing=self.use_checkpointing,
+            checkpointing_splits=self.num_checkpointing_splits,
+        )
 
-    def forward(self, image: torch.Tensor) -> torch.Tensor:
-        """
-        Args:
-            image: Input 4D tensor of shape `(N, in_chans, H, W)`.
+    # def forward(self, image: torch.Tensor) -> torch.Tensor:
+    #     """
+    #     Args:
+    #         image: Input 4D tensor of shape `(N, in_chans, H, W)`.
 
-        Returns:
-            Output tensor of shape `(N, out_chans, H, W)`.
-        """
-        stack = []
-        output = image
+    #     Returns:
+    #         Output tensor of shape `(N, out_chans, H, W)`.
+    #     """
+    #     stack = []
+    #     output = image
 
-        # apply down-sampling layers
-        for layer in self.down_sample_layers:
-            output = layer(output)
-            stack.append(output)
-            output = F.avg_pool2d(output, kernel_size=2, stride=2, padding=0)
+    #     # apply down-sampling layers
+    #     for layer in self.down_sample_layers:
+    #         output = layer(output)
+    #         stack.append(output)
+    #         output = F.avg_pool2d(output, kernel_size=2, stride=2, padding=0)
 
-        output = self.conv(output)
+    #     output = self.conv(output)
 
-        # apply up-sampling layers
-        for transpose_conv, conv in zip(self.up_transpose_conv, self.up_conv):
-            downsample_layer = stack.pop()
-            output = transpose_conv(output)
+    #     # apply up-sampling layers
+    #     for transpose_conv, conv in zip(self.up_transpose_conv, self.up_conv):
+    #         downsample_layer = stack.pop()
+    #         output = transpose_conv(output)
 
-            # reflect pad on the right/botton if needed to handle odd input dimensions
-            padding = [0, 0, 0, 0]
-            if output.shape[-1] != downsample_layer.shape[-1]:
-                padding[1] = 1  # padding right
-            if output.shape[-2] != downsample_layer.shape[-2]:
-                padding[3] = 1  # padding bottom
-            if torch.sum(torch.tensor(padding)) != 0:
-                output = F.pad(output, padding, "reflect")
+    #         # reflect pad on the right/botton if needed to handle odd input dimensions
+    #         padding = [0, 0, 0, 0]
+    #         if output.shape[-1] != downsample_layer.shape[-1]:
+    #             padding[1] = 1  # padding right
+    #         if output.shape[-2] != downsample_layer.shape[-2]:
+    #             padding[3] = 1  # padding bottom
+    #         if torch.sum(torch.tensor(padding)) != 0:
+    #             output = F.pad(output, padding, "reflect")
 
-            output = torch.cat([output, downsample_layer], dim=1)
-            output = conv(output)
+    #         output = torch.cat([output, downsample_layer], dim=1)
+    #         output = conv(output)
 
-        return output
+    #     return output
 
 
 class ConvBlock(nn.Module):
